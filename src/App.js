26bedc5398c67d8caddc84072ae2192b7fd1129d
Tiago Diniz <tmmidiatiago@gmail.com>
2021-03-14 18:19:02 -0300

    Pegando capas do Filmes e Séries do poster_path

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
 
 export default () => {
+
+  // Para salvar a lista de filmes que está vindo da requisição
+  const [movieList, setMovieList] = useState([]);
 
   useEffect(()=>{
     const loadAll = async () => {
       // Pegando a lista TOTAL
       let list = await Tmdb.getHomeList();
-      console.log(list);
+      //Mandando a lista que foi salva para a HOME
+      setMovieList(list);
+      
     }
 
     loadAll();
   }, []);
 
   return (
-    <div>
-      Netflix - Filmes e Séries
+    <div className="page">
+     <section className="lists">
+       {movieList.map((item, key)=>(
+        <MovieRow key={key}  title={item.title} item={item.items}/>
+       ))}
+     </section>
     </div>
   );
 }
