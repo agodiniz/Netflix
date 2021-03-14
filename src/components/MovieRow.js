6152de43331d1caec4d52c3e3c5d50d377edd568
Tiago Diniz <tmmidiatiago@gmail.com>
2021-03-14 18:47:31 -0300

    Estilização das categorias

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -3,14 +3,18 @@ import './MovieRow.css';
 
 export default ({title, item}) => {
   return (
-    <div>
+    <div className="movieRow">
       <h2>{title}</h2>
 
       <div className="movieRow--listarea">
-      {item?.results?.length > 0 && item?.results?.map((item, key)=> (
-      <img key={key} src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}/>
-    
-      ))}     
+        <div className="movieRow--list">
+          {item?.results?.length > 0 && item?.results?.map((item, key)=> (
+          <div key={key} className="movieRow--item">
+          <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}/>
+          </div>
+        ))} 
+        </div>
+          
         </div>
       </div>
   );
